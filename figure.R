42ae2467d79033b52f93a6b2d7e7cfd3b8b285b1
michbur <michalburdukiewicz@gmail.com>
2023-04-01 21:13:01 +0200

    updated text

--- figure.R
+++ figure.R
@@ -4,35 +4,33 @@ library(Cairo)
 
 font_add(family = "AlethiTS_lined", regular = "AlethiTS Fontv2/AlethiTS_lined.ttf")
 
-p <- ggplot() +
-  annotate("text", x = 1, y = 1, label = ".Journey before destination",
-           family = "AlethiTS_lined", size = 17) +
-  theme_void() +
-  theme(plot.background = element_rect(fill = NA, color = NA))
+showtext_auto()
+
+# p <- ggplot() +
+#   annotate("text", x = 1, y = 1, label = ".Journey before destination",
+#            family = "AlethiTS_lined", size = 17) +
+#   theme_void() +
+#   theme(plot.background = element_rect(fill = NA, color = NA))
+
+p <- ggplot(mtcars, aes(x = mpg, y = disp, color = cyl)) +
+  geom_point() +
+  theme_bw(base_family = "AlethiTS_lined")
 
 CairoPS("text.ps", height = 18, width = 18, bg = "transparent")
-showtext_begin()
 p
-showtext_end()
 dev.off()
 
-CairoPNG("text.png", height = 480*3, width = 480*3, bg = "transparent", 
-         pointsize = 10, res = 800)
-showtext_begin()
+CairoPNG("text.png", height = 480, width = 480, bg = "transparent", 
+         pointsize = 10, res = 100)
 p
-showtext_end()
 dev.off()
 
-CairoJPEG("text.jpeg", height = 480*3, width = 480*3, bg = "transparent", 
+CairoJPEG("text.jpeg", height = 480, width = 480, bg = "transparent", 
          pointsize = 10, res = 1800, quality = 160)
-showtext_begin()
 p
-showtext_end()
 dev.off()
 
-CairoTIFF("text.tiff", height = 480*3, width = 480*3, bg = "transparent", 
+CairoTIFF("text.tiff", height = 480, width = 480, bg = "transparent", 
           pointsize = 10, res = 1800, quality = 160)
-showtext_begin()
 p
-showtext_end()
 dev.off()
